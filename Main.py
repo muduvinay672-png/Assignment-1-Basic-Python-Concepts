6c0b392113fb27b13c52870fba3c4cf3acd4378a
muduvinay672-png <muduvinay672@gmail.com>
2025-09-21 09:56:13 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,16 +1,14 @@
 # #Task 1: Perform Basic Mathematical Operations
 
-# #Takes two numbers as input from the user.
-
-# num1 = int(input("Enter the first number: "))
-# num2 = int(input("Enter the second number: "))
+ num1 = int(input("Enter the first number: "))
+ num2 = int(input("Enter the second number: "))
 
 # #Performs the basic mathematical operations on these two numbers
 
-# print("Addition:", num1 + num2)
-# print("Subtraction:", num1 - num2)
-# print("Multiplication:", num1 * num2)
-# print("Division:", num1 / num2)
+ print("Addition:", num1 + num2)
+ print("Subtraction:", num1 - num2)
+ print("Multiplication:", num1 * num2)
+ print("Division:", num1 / num2)
 
 
 
@@ -18,5 +16,6 @@
 
 user_firstName = str(input("Enter your first name: "))
 user_lasttName = str(input("Enter your last name: "))
+
 
 print("Hello,",user_firstName , user_lasttName+"!", "Welcome to the Python program.")
