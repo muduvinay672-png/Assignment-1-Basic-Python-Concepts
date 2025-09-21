7372fe6d7d751c52f9c561e8430c37cd175173e0
muduvinay672-png <muduvinay672@gmail.com>
2025-09-21 09:52:44 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,22 @@
+# #Task 1: Perform Basic Mathematical Operations
+
+# #Takes two numbers as input from the user.
+
+# num1 = int(input("Enter the first number: "))
+# num2 = int(input("Enter the second number: "))
+
+# #Performs the basic mathematical operations on these two numbers
+
+# print("Addition:", num1 + num2)
+# print("Subtraction:", num1 - num2)
+# print("Multiplication:", num1 * num2)
+# print("Division:", num1 / num2)
+
+
+
+#Task 2: Create a Personalized Greeting
+
+user_firstName = str(input("Enter your first name: "))
+user_lasttName = str(input("Enter your last name: "))
+
+print("Hello,",user_firstName , user_lasttName+"!", "Welcome to the Python program.")
